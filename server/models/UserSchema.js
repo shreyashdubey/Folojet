dfae7174e8baaeb40d46755a86ad457c1674d64e
shreyashdubey <shreyashlrn@gmail.com>
2023-12-22 21:51:30 +0530

    add: User Model

---
+++ server/models/UserSchema.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, required: true, },
+  lastName: {type: String, required: true,},
+  email: { type: String, required: true, unique: true,  match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,},
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
