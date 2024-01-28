1804b8a468ae10620a1ac6e0ff876315ddd44fe0
shreyashdubey <shreyashlrn@gmail.com>
2024-01-28 11:53:15 +0530

    add: Latest Work

--- app.js
+++ app.js
@@ -1,47 +1,48 @@
-const path = require('path');
-const cors = require('cors');
-const cookieParser = require("cookie-parser")
-require('dotenv').config();
-const AppError = require('./utils/appError');
-const express = require('express');
-const http = require('http');
-const bodyParser = require('body-parser');
-const connectDB = require('./db');
+const path = require("path");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const AppError = require("./utils/appError");
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const connectDB = require("./db");
 const app = express();
 const server = http.createServer(app);
-const validateToken = require('./utils/validateToken');
-const userRoutes = require('./server/routes/UserRoutes')
-const fedxRoutes = require('./server/routes/FedxRoutes')
-const shopifyRoutes = require('./server/routes/ShopifyInstallationRoutes')
-const shopifyProducts = require('./server/routes/ShopifyProductsRoutes');
+const validateToken = require("./utils/validateToken");
+const userRoutes = require("./server/routes/UserRoutes");
+const fedxRoutes = require("./server/routes/FedxRoutes");
+const shopifyRoutes = require("./server/routes/ShopifyInstallationRoutes");
+const shopifyProducts = require("./server/routes/ShopifyProductsRoutes");
 app.use(cors());
 
-app.use((req, res, next) => {	// <- Serves req time and cookies
-	console.log(req.url)
-	req.requestTime = new Date().toISOString();
-	if (req.cookies) console.log(req.cookies);
-	next();
+app.use((req, res, next) => {
+  // <- Serves req time and cookies
+  console.log(req.url);
+  req.requestTime = new Date().toISOString();
+  if (req.cookies) console.log(req.cookies);
+  next();
 });
 
 app.use((req, res, next) => {
-	res.setHeader('Content-Type', 'application/json');
-	next();
+  res.setHeader("Content-Type", "application/json");
+  next();
 });
 
-app.use(express.json({ limit: '100mb' })); // <- Parses Json data
-app.use(express.urlencoded({ extended: true, limit: '100mb' })); // <- Parses URLencoded data
+app.use(express.json({ limit: "100mb" })); // <- Parses Json data
+app.use(express.urlencoded({ extended: true, limit: "100mb" })); // <- Parses URLencoded data
 
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use('/client/public/upload', express.static('/client/public/upload'));
-  
+app.use("/client/public/upload", express.static("/client/public/upload"));
+
 connectDB();
 
 //app.use(validateToken)
-app.use(shopifyRoutes);
-app.use('/api/products', shopifyProducts)
-app.use('/api/users', userRoutes);
-app.use('/api/fedx', fedxRoutes);
-console.log("App Started")
+app.use("/api/auth", shopifyRoutes);
+app.use("/api/products", shopifyProducts);
+app.use("/api/users", userRoutes);
+app.use("/api/fedx", fedxRoutes);
+console.log("App Started");
 
 module.exports = app;
