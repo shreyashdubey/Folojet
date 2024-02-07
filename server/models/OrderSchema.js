b247f2e728b9020c9ec8c709cf613cd1ad3ac9d8
shreyashdubey <shreyashlrn@gmail.com>
2024-02-07 21:47:25 +0530

    add: timestamps to orders

--- server/models/OrderSchema.js
+++ server/models/OrderSchema.js
@@ -1,12 +1,19 @@
 const mongoose = require("mongoose");
-const orderSchema = new mongoose.Schema({
-  id: { type: Number, required: true, unique: true },
-  admin_graphql_api_id: { type: String },
-  myshopify_domain: { type: String },
-  customerId: { type: mongoose.Schema.Types.ObjectId, ref: "CustomerSchema" },
-  fulfillments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Fulfillment" }],
-  line_items: [{ type: mongoose.Schema.Types.ObjectId, ref: "LineItem" }],
-});
+const orderSchema = new mongoose.Schema(
+  {
+    id: { type: Number, required: true, unique: true },
+    admin_graphql_api_id: { type: String },
+    myshopify_domain: { type: String },
+    customerId: { type: mongoose.Schema.Types.ObjectId, ref: "CustomerSchema" },
+    fulfillments: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "Fulfillment" },
+    ],
+    line_items: [{ type: mongoose.Schema.Types.ObjectId, ref: "LineItem" }],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Order = mongoose.model("Order", orderSchema);
 
